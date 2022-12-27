50065aba9f450ff7c52f1e4442e09b3f1278ea21
sahanenes <sahanenesss@gmail.com>
2022-12-27 22:15:19 +0300

    new

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from .views import (
+    todo_list_create,
+    todo_home,
+    todo_detail,
+    )
 
 urlpatterns = [
-    # path('', ),
+    path('list-create/',todo_list_create ),
    
 ]
